1a8cd8951cf7e392a82b01391044357555f751ed
For <for@darasa.io>
2025-08-06 13:59:05 +0700

    run-ubuntu

---
+++ run-ubuntu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 🧠 Index của instance, ví dụ: 0, 1, 2,...
+INDEX=$1
+PORT=$((9222 + INDEX))
+PROFILE_DIR="$HOME/chrome-bot-profile-$INDEX"
+CHROME_LOG=$(mktemp)
+
+echo "🚀 Starting Chrome instance #$INDEX on port $PORT..."
+
+google-chrome \
+  --remote-debugging-port=$PORT \
+  --remote-debugging-address=0.0.0.0 \
+  --remote-allow-origins=* \
+  --user-data-dir="$PROFILE_DIR" \
+  --no-first-run \
+  --no-default-browser-check \
+  --disable-popup-blocking \
+  --disable-default-apps \
+  --disable-background-networking \
+  --disable-sync \
+  --disable-translate > "$CHROME_LOG" 2>&1 &
+
+echo "⌛ Waiting for Chrome to print WebSocket URL..."
+while ! grep -q "DevTools listening on ws://" "$CHROME_LOG"; do
+  sleep 1
+done
+
+RAW_WS_URL=$(grep "DevTools listening on ws://" "$CHROME_LOG" | tail -n1 | awk '{print $NF}')
